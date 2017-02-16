b66e1ab182addf3e3b3c061cfecaf8c0b265973f
AndrewPopkov <otmorozok5000@yandex.ru>
2017-02-16 11:15:22 +0300

    1

---
+++ DimensionTest/datamodel/RootObject.cs
@@ -0,0 +1,194 @@
+﻿using DimensionTest.viewmodel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DimensionTest
+{
+
+    public class RootObject : NotifyPropertyChanged
+    {
+        public class Feature : NotifyPropertyChanged
+        {
+            public class Properties : NotifyPropertyChanged
+            {
+                public class CompanyMetaData : NotifyPropertyChanged
+                {
+                    public class Category : NotifyPropertyChanged
+                    {
+                        public string @class { get; set; }
+                        public string name { get; set { RaisePropertyChanged(() => name); } }
+                        public List<List<int>> nameHighlight { get; set; }
+                    }
+
+                    public class Phone : NotifyPropertyChanged
+                    {
+                        public string type { get; set; }
+                        public string formatted { get; set; }
+                        public string country { get; set; }
+                        public string prefix { get; set; }
+                        public string number { get; set; }
+                        public string info { get; set; }
+                    }
+
+                    public class Hours : NotifyPropertyChanged
+                    {
+                        public class Availability : NotifyPropertyChanged
+                        {
+
+                            public class Interval : NotifyPropertyChanged
+                            {
+                                public string from { get; set; }
+                                public string to { get; set; }
+                            }
+
+                            public bool Everyday { get; set; }
+                            public bool TwentyFourHours { get; set; }
+                            public List<Interval> Intervals { get; set; }
+                        }
+
+                        public List<Availability> Availabilities { get; set; }
+                        public string text { get; set; }
+                        public int tzOffset { get; set; }
+                    }
+
+                    public class Geo : NotifyPropertyChanged
+                    {
+                        public string precision { get; set; }
+                    }
+
+                    public class Feature : NotifyPropertyChanged
+                    {
+
+
+                        public class Value : NotifyPropertyChanged
+                        {
+                            public string id { get; set; }
+                            public string value { get; set; }
+                        }
+
+
+                        public string id { get; set; }
+                        public string type { get; set; }
+                        public string name { get; set; }
+                        public List<Value> values { get; set; }
+                        public object value { get; set; }
+                    }
+
+
+                    public class Snippet : NotifyPropertyChanged
+                    {
+                        public List<string> FeatureSet { get; set; }
+                    }
+
+                    public class Link : NotifyPropertyChanged
+                    {
+                        public string type { get; set; }
+                        public string aref { get; set; }
+                        public string href { get; set; }
+                    }
+
+                    public string id { get; set; }
+                    public string name { get; set; }
+                    public List<List<int>> nameHighlight { get; set; }
+                    public string address { get; set; }
+                    public List<List<int>> addressHighlight { get; set; }
+                    public string url { get; set; }
+                    public List<Category> Categories { get; set; }
+                    public List<Phone> Phones { get; set; }
+                    public Hours Hours { get; set; }
+                    public Geo Geo { get; set; }
+                    public List<Feature> Features { get; set; }
+                    public Snippet Snippet { get; set; }
+                    public List<Link> Links { get; set; }
+                }
+                public CompanyMetaData CompanyMetaData { get; set; }
+                public string description { get; set; }
+                public string name { get; set; }
+                public List<List<double>> boundedBy { get; set; }
+                public List<string> attributions { get; set; }
+            }
+
+            public class Geometry : NotifyPropertyChanged
+            {
+                public string type { get; set; }
+                public List<double> coordinates { get; set; }
+            }
+
+            public string type { get; set; }
+            public Properties properties { get; set; }
+            public Geometry geometry { get; set; }
+        }
+        public string type { get; set; }
+
+        public class Properties : NotifyPropertyChanged
+        {
+            public class Attribution : NotifyPropertyChanged
+            {
+                public class Sources : NotifyPropertyChanged
+                {
+                    public class Twitter : NotifyPropertyChanged
+                    {
+                        public class Author : NotifyPropertyChanged
+                        {
+                            public string name { get; set; }
+                            public string uri { get; set; }
+                        }
+                        public string id { get; set; }
+                        public Author author { get; set; }
+                    }
+
+                    public class Yandex : NotifyPropertyChanged
+                    {
+                        public class Author : NotifyPropertyChanged
+                        {
+                            public string name { get; set; }
+                            public string uri { get; set; }
+                        }
+                        public string id { get; set; }
+                        public Author author { get; set; }
+                    }
+                    public Twitter twitter { get; set; }
+                    public Yandex yandex { get; set; }
+                }
+                public Sources Sources { get; set; }
+            }
+            public class ResponseMetaData : NotifyPropertyChanged
+            {
+                public class SearchRequest : NotifyPropertyChanged
+                {
+                    public string request { get; set; }
+                    public int results { get; set; }
+                    public int skip { get; set; }
+                    public List<List<double>> boundedBy { get; set; }
+                }
+                public class SearchResponse : NotifyPropertyChanged
+                {
+                    public class Sort : NotifyPropertyChanged
+                    {
+                        public string type { get; set; }
+                    }
+
+                    public class Point : NotifyPropertyChanged
+                    {
+                        public string type { get; set; }
+                        public List<double> coordinates { get; set; }
+                    }
+                    public int found { get; set; }
+                    public Sort Sort { get; set; }
+                    public Point Point { get; set; }
+                    public List<List<double>> boundedBy { get; set; }
+                    public string display { get; set; }
+                }
+                public SearchRequest SearchRequest { get; set; }
+                public SearchResponse SearchResponse { get; set; }
+            }
+            public Attribution Attribution { get; set; }
+            public ResponseMetaData ResponseMetaData { get; set; }
+        }
+        public List<Feature> features { get; set; }
+
+    }
+
+}
---
+++ DimensionTest/view/myControls/mytexbox.xaml.cs
@@ -0,0 +1,112 @@
+﻿using DimensionTest.viewmodel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DimensionTest.view.myControls
+{
+    /// <summary>
+    /// Логика взаимодействия для mytexbox.xaml
+    /// </summary>
+    public partial class mytexbox : UserControl
+    {
+        private mytexboxViewModel viewmodel;
+        public mytexbox()
+        {
+            InitializeComponent();
+            viewmodel=new mytexboxViewModel();
+            this.datatext.GotFocus+= new RoutedEventHandler((o, e) =>
+            {
+
+            });
+            this.DataContext = viewmodel;
+
+        }
+        #region Text
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        public static readonly DependencyProperty
+            TextProperty = DependencyProperty.Register
+            ("Text",
+            typeof(string),
+            typeof(mytexbox),
+            new FrameworkPropertyMetadata(
+            string.Empty,
+            new PropertyChangedCallback(OnTextChanged))
+            );
+
+        public static void OnTextChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            var obj = o as mytexbox;
+            if (e.NewValue != null)
+            {
+
+            }
+        }
+        #endregion
+
+        #region Command
+        public string Command
+        {
+            get { return (string)GetValue(CommandProperty); }
+            set { SetValue(CommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty
+            CommandProperty = DependencyProperty.Register
+            ("Command",
+            typeof(string),
+            typeof(ICommand),
+            new FrameworkPropertyMetadata(null, OnCommandChanged)
+            );
+
+        public static void OnCommandChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            var obj = o as mytexbox;
+            if (e.NewValue != null)
+            {
+
+            }
+        }
+        #endregion
+        #region WatermarkText
+        public string WatermarkText
+        {
+            get { return (string)GetValue(WatermarkTextProperty); }
+            set { SetValue(WatermarkTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty 
+            WatermarkTextProperty = DependencyProperty.Register
+            ("WatermarkText", 
+            typeof(string),
+            typeof(mytexbox),
+            new FrameworkPropertyMetadata(string.Empty, OnWatermarkTextChanged)
+            );
+
+        public static void OnWatermarkTextChanged(DependencyObject o, DependencyPropertyChangedEventArgs e)
+        {
+            var obj = o as mytexbox;
+            if (e.NewValue != null)
+            {
+
+            }
+        }
+        #endregion
+    }
+}
